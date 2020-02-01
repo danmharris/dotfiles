6c2936d4697436d12afabf7db6d85a9394b14fcf
Dan Harris <danharris1606@gmail.com>
2020-02-01 20:28:47 +0000

    Add tmux and zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO: Install zsh, oh-my-zsh, base16-shell
+
 install -dm 0755 ~/.config/nvim/
 install -m 0644 nvim/init.vim ~/.config/nvim/
+install -m 0644 .tmux.conf ~/
+install -m 0644 .zshrc ~/
