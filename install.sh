324857f14016c770d36748ec307b68a807420c46
Dan Harris <danharris1606@gmail.com>
2020-10-04 17:13:36 +0100

    Update desktop configs

--- install.sh
+++ install.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
-# TODO: Install zsh, oh-my-zsh, base16-shell
+if [[ ! -e /etc/apt/sources.list.d/kgilmer-ubuntu-speed-ricer-focal.list ]]
+then
+    sudo add-apt-repository ppa:kgilmer/speed-ricer
+fi
+
+sudo apt install i3-gaps \
+                 i3lock \
+                 xss-lock \
+                 dmenu \
+                 feh \
+                 compton \
+                 polybar \
+                 breeze-cursor-theme \
+                 lxappearance \
+                 kitty \
+                 neovim \
+                 zsh \
 
 install -dm 0755 ~/.config/nvim/
 install -m 0644 nvim/init.vim ~/.config/nvim/
-install -m 0644 .tmux.conf ~/
-install -m 0644 .zshrc ~/
+
+install -m 0644 tmux/.tmux.conf ~/
+install -m 0644 zsh/.zshrc ~/
+
+install -dm 0755 ~/.config/polybar
+install -m 0644 polybar/* ~/.config/polybar/
+
+install -dm 0755 ~/.config/i3
+install -m 0644 i3/config ~/.config/i3/
+
+install -dm 0755 ~/.config/kitty
+install -m 0644 kitty/kitty.conf ~/.config/kitty/
+
+[[ $SHELL == $(which zsh) ]] || chsh -s $(which zsh)
