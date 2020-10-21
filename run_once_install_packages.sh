5503ecd88274b579861c48c42ef785f6d9adbe5c
Dan Harris <danharris1606@gmail.com>
2020-10-21 20:49:18 +0100

    Migrate to using chezmoi
    Seems to be a suitable tool for managing dotfiles across multiple
    machines where config may vary slightly.

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -18,21 +18,3 @@ sudo apt install i3-gaps \
                  kitty \
                  neovim \
                  zsh \
-
-install -dm 0755 ~/.config/nvim/
-install -m 0644 nvim/init.vim ~/.config/nvim/
-
-install -m 0644 tmux/.tmux.conf ~/
-install -m 0644 zsh/.zshrc ~/
-
-install -dm 0755 ~/.config/polybar
-install -m 0644 polybar/config ~/.config/polybar/
-install -m 0755 polybar/launch.sh ~/.config/polybar/
-
-install -dm 0755 ~/.config/i3
-install -m 0644 i3/config ~/.config/i3/
-
-install -dm 0755 ~/.config/kitty
-install -m 0644 kitty/kitty.conf ~/.config/kitty/
-
-[[ $SHELL == $(which zsh) ]] || chsh -s $(which zsh)
