e053cc097fe2d5eeccc086ecc743cffaa3ae5502
Dan Harris <danharris1606@gmail.com>
2020-11-15 18:52:58 +0000

    Only run install script on Ubuntu 20.04

--- run_once_install_packages.sh
+++ run_once_install_packages.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ ! -e /etc/apt/sources.list.d/kgilmer-ubuntu-speed-ricer-focal.list ]]
-then
-    sudo add-apt-repository ppa:kgilmer/speed-ricer
-fi
+if which lsb_release && [[ -n $(lsb_release -r | grep 20.04) ]]; then
+    echo 'Installing for Ubuntu 20.04'
+    if [[ ! -e /etc/apt/sources.list.d/kgilmer-ubuntu-speed-ricer-focal.list ]]
+    then
+        sudo add-apt-repository ppa:kgilmer/speed-ricer
+    fi
 
-sudo apt install i3-gaps \
-                 i3lock \
-                 xss-lock \
-                 dmenu \
-                 playerctl \
-                 feh \
-                 compton \
-                 polybar \
-                 breeze-cursor-theme \
-                 lxappearance \
-                 kitty \
-                 neovim \
-                 zsh \
-                 ripgrep \
+    sudo apt install i3-gaps \
+                     i3lock \
+                     xss-lock \
+                     dmenu \
+                     playerctl \
+                     feh \
+                     compton \
+                     polybar \
+                     breeze-cursor-theme \
+                     lxappearance \
+                     kitty \
+                     neovim \
+                     zsh \
+                     ripgrep
+fi
