1504fe4984296457ce2df79fdc1cd33d40843072
Dan Harris <danharris1606@gmail.com>
2022-10-08 16:41:34 +0100

    Add nvim-cmp

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -28,6 +28,44 @@ function configure_telescope()
   vim.keymap.set('n', 'fb', ':Telescope buffers<CR>')
 end
 
+function configure_cmp()
+  local cmp = require('cmp')
+
+  vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+    })
+  })
+
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+  local servers = { 'tsserver' }
+  for _,server in ipairs(servers) do
+    require('lspconfig')[server].setup {
+      capabilities = capabilities,
+    }
+  end
+end
+
 require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
 
@@ -56,6 +94,15 @@ require('packer').startup(function(use)
     },
     config = configure_telescope,
   }
+
+  use { 'neovim/nvim-lspconfig' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use {
+    'hrsh7th/nvim-cmp',
+    config = configure_cmp,
+  }
+  use { 'L3MON4D3/LuaSnip' }
+  use { 'saadparwaiz1/cmp_luasnip' }
 end)
 
 
