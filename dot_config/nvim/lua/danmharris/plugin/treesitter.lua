35f9b34da288fef380eef72776fba9d01dad498a
Dan Harris <danharris1606@gmail.com>
2023-10-04 18:10:11 +0100

    Enable treesitter highlighting

--- dot_config/nvim/lua/danmharris/plugin/treesitter.lua
+++ dot_config/nvim/lua/danmharris/plugin/treesitter.lua
@@ -2,4 +2,7 @@ require("nvim-treesitter.configs").setup({
   ensure_installed = {
     "beancount", "go", "hcl", "lua", "ruby", "vim", "vimdoc", "yaml"
   },
+  highlight = {
+    enable = true,
+  },
 })
