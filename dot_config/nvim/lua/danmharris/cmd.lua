f0991e2ac6939bdb4e42f566ff79daeff5dbdd94
Dan Harris <danharris1606@gmail.com>
2023-11-26 11:32:18 +0000

    Run lsp format on save

--- dot_config/nvim/lua/danmharris/cmd.lua
+++ dot_config/nvim/lua/danmharris/cmd.lua
@@ -1,6 +1,10 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("BufWritePre", {
+  callback = function() vim.lsp.buf.format() end
+})
+
+autocmd("BufWritePre", {
   pattern = "*",
   command = "%s/\\s\\+$//e"
 })
