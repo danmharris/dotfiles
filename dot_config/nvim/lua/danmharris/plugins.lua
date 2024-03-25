f9dbe8398e8543fa789b510d39cd7199475f6b0c
Dan Harris <danharris1606@gmail.com>
2024-03-25 17:47:29 +0000

    lsp changes and autocomplete

--- dot_config/nvim/lua/danmharris/plugins.lua
+++ dot_config/nvim/lua/danmharris/plugins.lua
@@ -77,21 +77,36 @@ require("lazy").setup({
         lazy = true,
     },
     {
-        "williamboman/mason.nvim",
-        config = true,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = "BufEnter",
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+        },
         config = function()
-            local lspconfig = require("lspconfig")
+            local lsp_zero = require('lsp-zero')
 
-            lspconfig.elixirls.setup({
-                cmd = { "elixir-ls" },
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
+
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ansiblels",
+                    "elixirls",
+                    "lua_ls",
+                    "gopls",
+                    "solargraph",
+                },
+                handlers = {
+                    lsp_zero.default_setup,
+                },
             })
-            lspconfig.gopls.setup({})
-            lspconfig.solargraph.setup({})
-            lspconfig.ansiblels.setup({})
         end,
     },
     {
